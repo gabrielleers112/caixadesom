761e51299953a2527fd29784b2a8cd564ae8f488
gabrielle <105082188+gabrielleers112@users.noreply.github.com>
2023-10-27 10:28:02 -0300

    v

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-document.querySelector('.tecla_cavalo').play();
+//declaração de função tocasomcava
+ function tocacava () {
+document.querySelector('#som_tecla_cava').play();
+ }
+
+ //criando a referência constante listadeteclas e buscando a classe coletiva . tecla
+ const listadeteclas = document.querySelectorAll('.tecla');
+ 
+
+ let contador=0;
+ while(contador < 9){
+listadeteclas[0].onclick=tocacava;
+contador = contador + 1;
+console.log(contador)
+ }
